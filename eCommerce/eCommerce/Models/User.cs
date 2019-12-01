8a1947e450e168316e4cf8fa218d744e34f840ba
AndriyPlekan <44374776+AndriyPlekan@users.noreply.github.com>
2019-12-01 21:22:59 +0200

    Created Registration

--- eCommerce/eCommerce/Models/User.cs
+++ eCommerce/eCommerce/Models/User.cs
@@ -1,13 +1,16 @@
-namespace eCommerce.Models
+ namespace eCommerce.Models
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using eCommerce.Models;
+    using System.ComponentModel;
 
     public partial class User
     {
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public User()
         {
@@ -18,16 +21,22 @@ namespace eCommerce.Models
 
         public int userId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
         [StringLength(50)]
         public string username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Password)]
         [StringLength(50)]
         public string password { get; set; }
 
-        public bool isAdmin { get; set; }
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [DisplayName ("Confirm Password") ]
+        public string confirmPassword { get; set; }
 
+        public bool isAdmin { get; set; }
+        
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Comment> Comments { get; set; }
 
